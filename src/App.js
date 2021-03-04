33c993b1736708c00c4fb3ff0482320d5a4475c1
unknown <ddaksh.2711@gmail.com>
2021-03-04 15:36:01 +0530

    Add smooth-scroll

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Body from './components/Body';
+import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
 function App() {
   return (
     <div className="App">
       <Body />
+      <hr className="hr1"></hr>
       <hr className="hr2"></hr>
       <hr className="hr3"></hr>
       <hr className="hr4"></hr>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './styles/navbar.css';
+import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
 function NavBar(){
     return (
-        <div className="navigation-bar">
+        <div className="navigation-bar" id="navbar">
             <ul className="navlist">
-                <li><a href="https://www.figma.com/file/CqWILXDHiTlMGcXnPIU3zQ/Portfolio?node-id=0%3A3">Home</a></li>
-                <li><a href="https://www.figma.com/file/CqWILXDHiTlMGcXnPIU3zQ/Portfolio?node-id=0%3A3">About</a></li>
-                <li><a href="https://www.figma.com/file/CqWILXDHiTlMGcXnPIU3zQ/Portfolio?node-id=0%3A3">Projects</a></li>
-                <li><a href="https://www.figma.com/file/CqWILXDHiTlMGcXnPIU3zQ/Portfolio?node-id=0%3A3">Skills</a></li>
-                <li><a href="https://www.figma.com/file/CqWILXDHiTlMGcXnPIU3zQ/Portfolio?node-id=0%3A3">Contact</a></li>
+                <li><a onClick={() => scroll.scrollToTop()}>Home</a></li>
+                <li><a onClick={() => scroll.scrollMore(1010)}>About</a></li>
+                <li><a onClick={() => scroll.scrollMore(2010)}>Projects</a></li>
+                <li><a onClick={() => scroll.scrollMore(3010)}>Skills</a></li>
+                <li><a onClick={() => scroll.scrollToBottom()}>Contact</a></li>
             </ul>
         </div>
     )
